028fb83545dedc726dbbbac7f3962d81d3782a13
mgradalska <martagradalska@gmail.com>
2019-07-27 15:38:24 +0200

    Change db settings

--- weirdtext/settings.py
+++ weirdtext/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -118,3 +119,5 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
